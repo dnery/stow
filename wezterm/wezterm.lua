e0b4fd5caca3134d8587c419682f565e6492d0ec
Danilo <dznery@gmail.com>
2024-12-02 09:58:15 +0530

    Some config updates, not entirely sure what's going on

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+-- local config = wezterm.config_builder()
+
+-- DO CONFIG HERE
+
+-- config.font = wezterm.font 'Jetbrains Mono'
+config.color_scheme = 'Solarized Dark Higher Contrast'
+
+return config
