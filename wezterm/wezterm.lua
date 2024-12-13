8c78a2130a0d85f648c43df12edf1823a907a533
Danilo <danilo@enlyft.com>
2024-12-13 20:40:05 +0530

    Update wezterm config & KPXC startup options

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,16 @@ local wezterm = require 'wezterm'
 local config = {}
 -- local config = wezterm.config_builder()
 
--- DO CONFIG HERE
+-- DO MORE CONFIG HERE
 
+-- Colors and font
 -- config.font = wezterm.font 'Jetbrains Mono'
 config.color_scheme = 'Solarized Dark Higher Contrast'
+
+-- Remove annoying update banners
+config.check_for_updates = false
+
+-- Compositing options
+config.window_background_opacity = 0.75
 
 return config
