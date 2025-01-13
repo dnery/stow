adbfb0fa64a2f41d269a0f197178663eb14c93a6
Danilo <dznery@gmail.com>
2025-01-13 03:49:21 -0300

    Remove cruft, massively improve install script

--- install.sh
+++ install.sh
@@ -5,43 +5,77 @@ set -eo pipefail
 cwd=$(dirname "$0")
 cd "$cwd"
 
-echo "Stowing on ~/.config"
+if [ $(id -u) -ne 0 ]; then :; else
+	echo "Do NOT run this as root"
+	exit 1
+fi
 
-stow .
-
-
-echo "Stowing uncool app configs"
-(
-	cd uncool;
+read -p "Stow all to ~/.config? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	stow .
-)
+else
+	echo "Skipping..."
+fi
 
-echo "Populating global linux box configs"
+read -p "Stow uncool configs? " -n 1 -r 
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	(
+		cd uncool;
+		stow .
+	)
+else
+	echo "Skipping..."
+fi
 
-set -- "security/pam_env.conf" "sudoers.d/danilo"
+read -p "Populate /etc files? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	set -- "security/pam_env.conf" "sudoers.d/danilo"
+	for it in "$@"; do
+		if [ -e "/etc/${it}" ]; then
+			echo "/etc/${it} already exists"
 
-for it in "$@"; do
-	if [ -e "/etc/${it}" ]; then
-		echo "/etc/${it} already exists"
-	fi
-	if [ $(id -u) -ne 0 ]; then
-		sudo cp -i "${cwd}/etc/${it}" "/etc/${it}"
-	else
-		cp -i "${cwd}/etc/${it}" "/etc/${it}"
-	fi
-done
+			if [ ! -e "/etc/${it}.bak" ]; then
+				read -p "Back up the existing ${it}? " -n 1 -r
+				echo
+				if [[ $REPLY =~ ^[Yy]$ ]]; then
+					sudo cp "/etc/${it}" "/etc/${it}.bak"
+				fi
+			else
+				echo "Back up already exist, will not override"
+			fi
 
-echo "Doing the thingy with KeepassXC ssh keys"
+		fi
+		read -p "Copy the custom config to ${it}? " -n 1 -r
+		echo
+ 		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			sudo cp "${cwd}/etc/${it}" "/etc/${it}"
+		fi
 
-kdbx="${HOME}/Sync/Secrets/Workstation.kdbx"
-keys=( edge blaze deploy )
+	done
+else
+	echo "Skipping..."
+fi
 
-for key in "${keys[@]}"; do
-	keyfile="${HOME}/.ssh/${key}.pub"
-	if [[ ! -f $keyfile || ! -r $keyfile ]]; then
-		keepassxc-cli show -a Notes $kdbx $key > $keyfile
+
+read -p "Do the thingy with KeepassXC ssh keys? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	kdbx="${HOME}/Sync/Secrets/Workstation.kdbx"
+	keys=( edge blaze deploy )
+
+	read -p "Input your password: " -n 12 -r
+	echo
+	for key in "${keys[@]}"; do
+		keyfile="${HOME}/.ssh/${key}.pub"
+		keepassxc-cli show -a Notes $kdbx $key > $keyfile <<< "$REPLY"
 		chmod 644 $keyfile
-	fi
-done
+	done
+else
+	echo "Skipping..."
+fi
+
 
 echo "Done."
